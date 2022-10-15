d5ac83950ae828ba385e504c09601c280a8849c2
MariaF <mariaeugenia.fiorentino@gmail.com>
2022-10-14 22:04:12 -0300

    validacion formulario contacto y reserva

---
+++ app.js
@@ -0,0 +1,42 @@
+function validacion(valor){
+  //  valor = document.getElementById("nombre").value;
+    let text;
+
+    if(!(/^[a-zA-Z]+$/.test(valor)))
+    {
+        return false    
+    } 
+    
+    return true
+      
+}
+function controlemail(email){
+    if (!(/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/.test(email))){
+        
+       return false
+}
+    return true
+}
+function inicio(){
+    var valor= new Array();
+    valor[0]= document.getElementById("nombre").value;
+    valor[1]= document.getElementById("apellido").value;
+     
+    for(i=0; i<valor.length;i++)
+    {
+       if(!validacion(valor[i])){
+            if(i==0){
+                window.alert("Ingreso mal el nombre")
+            }else{ 
+                window.alert("Ingreso mal el apellido")
+            }
+       }
+    }
+        if(!controlemail(document.getElementById("email").value)){
+            window.alert("El mail es incorrecto")
+
+        }
+        
+}
+
+                     
